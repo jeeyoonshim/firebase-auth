e2c3c73965675edd33713c1a9ffafeddd1ef9814
Jeeyoon (Janet) Shim <127158088+jeeyoonshim@users.noreply.github.com>
2023-08-12 18:52:30 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,109 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "https://www.gstatic.com/firebasejs/9.9.1/firebase-auth.js";
+
+
+(function () {
+  // Your web app's Firebase configuration, get it from your firebase project settings page on the General tab.
+  const firebaseConfig = {
+    apiKey: "AIzaSyBVpBDM3_gNrmnbKdVpfj7N4IiosX6_p5M",
+    authDomain: "auth-proj-86bd5.firebaseapp.com",
+    projectId: "auth-proj-86bd5",
+    storageBucket: "auth-proj-86bd5.appspot.com",
+    messagingSenderId: "502277148022",
+    appId: "1:502277148022:web:670614f908adddae33be57"
+  };
+  // Initialize Firebase
+  // firebase.initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+  // Initialize Firebase Authentication and get a reference to the service
+  const auth = getAuth(app);
+
+  // TODO: initialize provider for google auth
+  const provider = new GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');``
+  console.log("app initialized...");
+
+  // get elements
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const login = document.getElementById("login");
+  const signup = document.getElementById("signup");
+  const logout = document.getElementById("logout");
+  const loggedInStatus = document.getElementById("loggedInStatus");
+  const googlelogin = document.getElementById("googlelogin");
+
+  // login
+  login.addEventListener("click", (e) => {
+    signInWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+        // Signed in
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  // signup
+  signup.addEventListener("click", (e) => {
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+        // Signed in
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  //TODO: Add Google Sign in
+  googlelogin.addEventListener("click", (e) => {
+    console.log("google sign in clicked");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    // TODO: Use signInWithPopup to implement Google sign in
+    // Hint: refer to instructions in the official google documentation: https://firebase.google.com/docs/auth/web/google-signin
+  });
+
+
+  // logout
+  logout.addEventListener("click", (e) => {
+    auth.signOut();
+  });
+
+  // login state
+  auth.onAuthStateChanged((firebaseUser) => {
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      loggedInStatus.innerText = `You are logged in using the following email: ${firebaseUser.email}`;
+      logout.style.display = "inline";
+      login.style.display = "none";
+      signup.style.display = "none";
+      email.style.display = "none";
+      password.style.display = "none";
+      googlelogin.style.display = "none";
+    } else {
+      console.log("User is not logged in");
+      loggedInStatus.innerText = "You are not yet logged in";
+      login.style.display = "inline";
+      signup.style.display = "inline";
+      email.style.display = "inline";
+      googlelogin.style.display = "inline";
+      password.style.display = "inline";
+      logout.style.display = "none";
+    }
+  });
+})();
